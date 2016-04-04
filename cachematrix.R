29a7f69439211c5610b6bfc991e021597baf2f0c
Alexis James Corbet Hughes <alexisjchughes@hotmail.com>
2016-04-04 07:23:57 +0100

    modified by alex hughes, for assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## matrix inversions are computationally expensive, so if a matrix remains unchanged but we require it's
+## inverse many times we could store it in a separate variable and always remember to update that variable when the
+## matrix is changed in our code. However it is more elegant to create an object hat stores both the matrix and it's
+## inverse (if it has ever been calculated) and automatically recalculate whenever the value is changed.
+## 
 
-## Write a short comment describing this function
-
+## this function appears to return a matrix but it actually reurns is a special list of functions similar to a c# class
+## or more precisely the c# get; set; accessors on a class variable. as get and set are reserved words when we call 
+## ma <- makecacheMatrix and type m a matrix is returned (via the get accessor)
+## 
 makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  
+  get <- function() x
+  setinverse <- function(inverse) inv <- inverse
+  getinverse <- function() inv
+  list(set = set, get=get, setinverse=setinverse, getinverse=getinverse)
 
 }
-
-
-## Write a short comment describing this function
-
+##cacheSolve call the getinverse and setinverse "methods" of the cacheMatrix Object and if the inverse has not yet been calculated
+## it updates it.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinverse(inv)
+  inv
 }
+
+
+
